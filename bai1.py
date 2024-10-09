db5c37a7cc1d7c19445f0a95e3240567ee18c3f6
minhdcdev1308 <23110267@student.hcmute.edu.vn>
2024-10-09 22:56:39 +0700

    Add files via upload

--- bai1.py
+++ bai1.py
@@ -1,18 +1,8 @@
-from math import pi
-
-class circle:
-    def __init__ (self,ban_kinh):
-        self.ban_kinh = ban_kinh
-    def chuvi(self):
-        return 2*pi*self.ban_kinh
-    def dientich(self):
-        return pi * self.ban_kinh * self.ban_kinh
-    
-    def thongtin(self):
-        print(f"Day la hinh tron co ban kinh: {self.ban_kinh}")
-
-
-ht1 = circle(5)
-print(ht1.chuvi())
-print(ht1.dientich())
-print(ht1.thongtin())
+price = input("Nhap don gia: ")
+size = input("Nhap so luong: ")
+bef = int(price) * int(size)
+thue = bef / 10
+aft = bef + thue
+print('{0:>26} {1:>15}' .format("Tong tien truoc thue: ", str(price) + '*' + str(size) + '=' + str(bef)))
+print('{0:>26} {1:>15}' .format("Tien thue: ", thue))
+print('{0:>26} {1:>15}' .format("Tong tien sau thue: ", aft))
